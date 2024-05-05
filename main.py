9a06935eee19fc7a50825110641b1a82dc3bb079
Archit Sharma <architsh007@gmail.com>
2024-05-05 20:01:40 +0530

    most working code here but not giving last page output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import httpx
 from selectolax.parser import HTMLParser
 import time
+from urllib.parse import urljoin
 
 
 def get_response(url):
@@ -43,9 +44,16 @@ def extract_text(selector):
         extract the text from the css selector and returns text from it otherwise returns None
     """
     try:
-        return selector
+        return selector.text()
     except AttributeError:
         return None
+
+
+def extract_full_price(product):
+    if extract_text(product.css_first("span[data-ui=full-price]")) is not None:
+        return extract_text(product.css_first("span[data-ui=full-price]"))
+    else:
+        return extract_text(product.css_first("span[data-ui=compare-at-price]"))
 
 
 def product_details(product):
@@ -53,12 +61,21 @@ def product_details(product):
         extract deatils of the products from the results and prints the result
     """
     item = {
-        "Brand" : extract_text(product.css_first("span[data-ui=product-brand]").text()),
-        "Name of product" : extract_text(product.css_first("span[data-ui=product-title]").text()),
-        "Sale price" : extract_text(product.css_first("span[data-ui=sale-price]").text()),
-        "Full price" : extract_text(product.css_first("span[data-ui=full-price]").text())
+        "Brand" : extract_text(product.css_first("span[data-ui=product-brand]")),
+        "Name of product" : extract_text(product.css_first("span[data-ui=product-title]")),
+        "Sale price" : extract_text(product.css_first("span[data-ui=sale-price]")),
+        "Full price" : extract_full_price(product)
     }
     return item
+
+
+def next_page(html):
+    if html.css_first("a[data-id=pagination-test-link-next]") == None:
+        print("This is the last page")
+        exit()
+    else:
+        time.sleep(2)
+        return urljoin("https://www.rei.com/c/mens-clothing/f/scd-deals", html.css_first("a[data-id=pagination-test-link-next]").attributes["href"]) 
 
 
 def main():
@@ -66,12 +83,18 @@ def main():
     url_to_scrape = "https://www.rei.com/c/mens-clothing/f/scd-deals"
     
     html = prase_page(get_response(url_to_scrape))
-    
-    products = select_products(html)
-    
-    for product in products:
-        print(extract_text(product_details(product)))
+    next_url = next_page(html)
+    counter = 1
 
+    while next_url is not None:
+        next_url = next_page(html)
+        products = select_products(html)        
+        print("\nGathering product information on page", counter)    
+        for product in products:
+            print(product_details(product))
+        html = prase_page(get_response(next_url))
+        counter += 1
+    
 
 if __name__ == "__main__":
     main()
